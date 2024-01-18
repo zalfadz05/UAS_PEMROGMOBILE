129c7abf6dbd370ae5876c08e78c6a8e43570cc7
Zalfa Dewi Zahrani <zalfaadz04@gmail.com>
2024-01-19 01:18:42 +0700

    upload

---
+++ pemrograman_mobile/app/src/main/java/com/pemrograman_mobile/MainMaps.java
@@ -0,0 +1,39 @@
+package com.pemrograman_mobile;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        map = findViewById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+
+        LatLng mapIndonesia = new LatLng(-6.241306,107.157250);
+        this.gMap.addMarker(new MarkerOptions().position(mapIndonesia).title("Kantor ArrasyNET"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapIndonesia));
+    }
+}
